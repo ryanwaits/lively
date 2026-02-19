68f19fc41ead09377a447738881aa65120d9a3f5
Ryan Waits <ryan.waits@gmail.com>
2026-02-19 02:20:35 -0600

    chore: remove Next.js whiteboard app from monorepo root

--- src/types/undo.ts
+++
@@ -1,6 +0,0 @@
-import type { BoardObject } from "./board";
-
-export type UndoEntry =
-  | { type: "create"; objects: BoardObject[] }
-  | { type: "delete"; objects: BoardObject[] }
-  | { type: "update"; before: BoardObject[]; after: BoardObject[] };
